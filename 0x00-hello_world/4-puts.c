69d915798714a9e3438583162bc912097ac2b505
Ahmed24ibrahim <ahmedibrahim0945@gmail.com>
2023-09-07 19:32:51 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 /**
 *main - Entry point
+*
+*Description: using sizeof to print the size of various types.
+*
 *return: Always 0 (Success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
