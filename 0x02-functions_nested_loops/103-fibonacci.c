0d11193f1e3142fe925680107f1dc984ee70c17e
Ahmed24ibrahim <ahmedibrahim0945@gmail.com>
2023-09-12 19:00:54 -0700

    a

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
-
 /**
-  * main - the main printing funtion
-  * less than 4000000.
-  * Return: 0.
-  */
+ * This function calculates the sum of even-valued Fibonacci numbers up to a given limit.
+ *
+ * @param limit The upper limit for Fibonacci numbers.
+ * @return The sum of even-valued Fibonacci numbers up to the limit.
+ */
+int sumEvenFibonacci(int limit) {
+    int a = 1, b = 2, sum = 0;
 
+    while (b <= limit) {
+        if (b % 2 == 0) {
+            sum += b;
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
 
-int main(void)
-{
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+    return sum;
+}
 
+int main() {
+    int limit = 4000000;
+    int result = sumEvenFibonacci(limit);
 
-	while (k + j < 4000000)
-	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
-	}
-	printf("%ld", sum);
-	return (0);
+    printf("The sum of even-valued Fibonacci terms not exceeding %d is: %d\n", limit, result);
+
+    return 0;
 }
